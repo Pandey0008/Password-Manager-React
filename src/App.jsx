8121a99a14c1c65d4bd28f8b4b5d58f32f96e814
Shivansh Pandey <mailatshivm@gmail.com>
2025-02-25 17:41:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react' 
-import './App.css'
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
 import Footer from './components/Footer'
 
-function App() { 
-
+function App() {
   return (
-    <>
-        <Navbar /> 
-        <div className="bg-b-50 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar */}
+      <Navbar />
 
-       <Manager/> 
-        </div>
-       <Footer/>
-    </>
-  )
+      {/* Main Content with Background Gradient */}
+      <main className="min-h-screen flex flex-col justify-center items-center 
+bg-[radial-gradient(ellipse_at_center,rgba(120,119,198,0.3),rgba(255,255,255,0))] 
+px-4 sm:px-6">
+
+        <Manager />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
